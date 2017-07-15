e25b732582b583fbc7eb79d71e8959f0158f29a1
ivan135 <1576475704@qq.com>
2017-07-15 15:32:00 +0800

    注册登录功能的实现

--- app.js
+++ app.js
@@ -1,31 +1,81 @@
 import Vue from 'vue'
+import AV from 'leancloud-storage'
+
+let APP_ID = '8axnRtGoxCJhEzsvNPEAHnol-gzGzoHsz';
+let APP_KEY = '0YH4XkYflb4CUPfA743TGj8G';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
 
 var app = new Vue({
   el: '#app',
   data: {
+    actionType: 'signUp',
+    formData: {
+      username: '',
+      password: ''
+    },
     newTodo: '',
-    todoList: []
+    todoList: [],
+    currentUser: null,
   },
-    created: function(){
-    // onbeforeunload文档：https://developer.mozilla.org/zh-CN/docs/Web/API/Window/onbeforeunload
-    window.onbeforeunload = ()=>{
-      let dataString = JSON.stringify(this.todoList) // JSON 文档: https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/JSON
-      window.localStorage.setItem('myTodos', dataString) // 看文档https://developer.mozilla.org/zh-CN/docs/Web/API/Window/localStorage
+  created: function(){
+       window.onbeforeunload = ()=>{
+      let dataString = JSON.stringify(this.todoList) 
+      window.localStorage.setItem('myTodos', dataString) 
     }
 
     let oldDataString = window.localStorage.getItem('myTodos')
     let oldData = JSON.parse(oldDataString)
     this.todoList = oldData || []
 
+    this.currentUser = this.getCurrentUser();
+
   },
   methods: {
-    addTodo: function () {
+    addTodo: function(){
       this.todoList.push({
         title: this.newTodo,
         createdAt: new Date(),
-        done:false
+        done: false // 添加一个 done 属性
       })
-      this.newTodo='';
+      this.newTodo = ''
+    },
+    removeTodo: function(todo){
+      let index = this.todoList.indexOf(todo) 
+      this.todoList.splice(index,1) 
+    },
+    signUp: function () {
+      let user = new AV.User();
+      user.setUsername(this.formData.username);
+      user.setPassword(this.formData.password);
+      user.signUp().then((loginedUser) => {
+        this.currentUser = this.getCurrentUser()
+      }, (error) => {
+        alert('注册失败')
+      });
+    },
+    login: function () {
+      AV.User.logIn(this.formData.username, this.formData.password).then((loginedUser) => {
+        this.currentUser = this.getCurrentUser()
+      }, function (error) {
+        alert('登录失败')
+      });
+    },
+    getCurrentUser: function () {
+      let current = AV.User.current()
+      if (current) {
+        let {id, createdAt, attributes: {username}} = current
+        return {id, username, createdAt}
+      } else {
+        return null
+      }
+    },
+    logout: function () {
+      AV.User.logOut()
+      this.currentUser =null
+      window.location.reload()
     }
   }
-})
+})     
